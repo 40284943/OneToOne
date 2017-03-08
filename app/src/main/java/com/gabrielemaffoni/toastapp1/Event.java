5fdd2e3374f6854947dc1281128fc6a631c96fc7
Gabriele Maffoni <40284943@live.napier.ac.uk>
2017-03-08 14:07:04 +0000

    Version 1.1

--- app/src/main/java/com/gabrielemaffoni/toastapp1/Event.java
+++ app/src/main/java/com/gabrielemaffoni/toastapp1/Event.java
@@ -1,7 +1,10 @@
 package com.gabrielemaffoni.toastapp1;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
+import com.gabrielemaffoni.toastaapp1.R;
 
 /**
  * Created by gabrielemaffoni on 08/03/2017.
@@ -12,6 +15,8 @@ public class Event extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.single_event_layout);
 
+        ViewPager pager = (ViewPager) findViewById(R.id.cards);
     }
 }
---
+++ app/src/main/java/com/gabrielemaffoni/toastapp1/WhatCard.java
@@ -0,0 +1,22 @@
+package com.gabrielemaffoni.toastapp1;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gabrielemaffoni.toastaapp1.R;
+
+/**
+ * Created by gabrielemaffoni on 08/03/2017.
+ */
+
+public class WhatCard extends Fragment {
+
+    @Override
+    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceSatate){
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.what_card,container,false);
+        return rootView;
+    }
+}
